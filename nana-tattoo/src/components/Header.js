115cd91971ddfdc66b0fe14110776fcb279c3a1e
Archie To <toanhminh0412@gmail.com>
2024-04-06 14:21:09 -0700

    Add 2 action buttons

--- nana-tattoo/src/components/Header.js
+++ nana-tattoo/src/components/Header.js
@@ -1,6 +1,8 @@
 "use client";
 
+import Link from "next/link";
 import { motion } from "framer-motion"
+import { PopLink } from "./motions/Links";
 
 export default function Header() {
     return (
@@ -21,6 +23,17 @@ export default function Header() {
                     className="mt-6 text-lg leading-8 text-gray-300">
                     Come visit me at The Blue Rabbit Tattoo & Beauty Bar. I have 5 years of experience in Inkjet and permanent tattoos. I will make your body a walking art.
                 </motion.p>
+                <div className="mt-10 flex items-center justify-center gap-x-6">
+                    <Link
+                    href="/works"
+                    className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+                    >
+                    My works
+                    </Link>
+                    <PopLink href="#" className="text-sm font-semibold leading-6 text-white">
+                    Book now <span aria-hidden="true">→</span>
+                    </PopLink>
+                </div>
             </div>
         </section>
     )
