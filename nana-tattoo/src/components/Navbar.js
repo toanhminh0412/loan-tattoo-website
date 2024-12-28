5b8e503a3e2957f7aa25dacdf3ec851674243918
Archie To <toanhminh0412@gmail.com>
2024-12-28 14:33:59 -0800

    Add missing FAQs item

--- nana-tattoo/src/components/Navbar.js
+++ nana-tattoo/src/components/Navbar.js
@@ -18,6 +18,8 @@ export default function Navbar() {
 
   return (
     <div className="bg-gray-900">
+
+        {/* Desktop navbar */}
         <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
             <div className="flex lg:flex-1">
             <PopLink href="/" className="-m-1.5 p-1.5 text-white font-semibold text-xl">
@@ -53,6 +55,8 @@ export default function Navbar() {
             </PopLink>
             </div>
         </nav>
+
+        {/* Mobile nav */}
         <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
             <div className="fixed inset-0 z-10" />
             <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
@@ -81,6 +85,13 @@ export default function Navbar() {
                         {item.name}
                     </JumpLink>
                     ))}
+                    <JumpLink
+                        key="FAQs"
+                        href="/faqs"
+                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-800"
+                    >
+                        FAQs
+                    </JumpLink>
                 </div>
                 <div className="py-6">
                     <PopLink
