6add64d6974d4727ac493b355f28fcdf0b5a537b
Archie To <toanhminh0412@gmail.com>
2024-12-29 14:05:48 -0800

    Remove FAQs section from the home page

--- nana-tattoo/src/app/page.js
+++ nana-tattoo/src/app/page.js
@@ -1,13 +1,11 @@
 import Header from "../components/Header";
 import PermanentMakeup from "@/components/PermanentMakeup";
 import Portfolio from "../components/Portfolio";
-import FAQs from "@/components/FAQs";
 
 export default function Home() {
     return (
         <>
             <Header/>
-            <FAQs/>
             <PermanentMakeup/>
             <Portfolio/>
         </>
